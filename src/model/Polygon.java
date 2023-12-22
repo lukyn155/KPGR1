7d3d3f8c71d93f21e352f35e46459ae8baffd105
Lukas Matejovsky <lukyn.ma@gmail.com>
2023-12-22 17:32:58 +0100

    Úprava třídy Polygon -> přidání tečkované čáry

--- src/model/Polygon.java
+++ src/model/Polygon.java
@@ -1,10 +1,12 @@
 package model;
 
+import java.awt.*;
 import java.util.ArrayList;
+import java.util.List;
 
 public class Polygon {
     //Zde bude list se zadanými body
-    private ArrayList<Point> points = new ArrayList<>();
+    protected ArrayList<Point> points = new ArrayList<>();
 
     public Point getPoint(int index) {
         return points.get(index);
@@ -13,12 +15,17 @@ public class Polygon {
     public Polygon() {
 
     }
+
     public Polygon(ArrayList<Point> points) {
         this.points = points;
     }
 
     public void addPoint(Point point) {
         this.points.add(point);
+    }
+
+    public Point getBeforeLastPoint() {
+        return points.get(points.size() - 2);
     }
 
     public void replaceLastPoint(Point point) {
@@ -28,6 +35,24 @@ public class Polygon {
     public ArrayList<Point> getPoints() {
         return points;
     }
-//TODO
 
+    public void setPoints(ArrayList<Point> points) {
+        this.points = points;
+    }
+
+    public ArrayList<Line> getLines() {
+        ArrayList<Line> lines = new ArrayList<>();
+        for (int i = 0; i < points.size(); i++) {
+            int nextIndex = (i + 1) % points.size();
+            lines.add(
+                    new Line(
+                            points.get(i).x,
+                            points.get(nextIndex).x,
+                            points.get(i).y,
+                            points.get(nextIndex).y,
+                            Color.yellow.getRGB()));
+//            System.out.println(points.get(i).x + " " + points.get(nextIndex).x + " " + points.get(i).y + " " + points.get(nextIndex).y);
+        }
+        return lines;
+    }
 }
